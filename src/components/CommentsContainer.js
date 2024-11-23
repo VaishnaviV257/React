421c46c28170357493e5032d5c8fb7edd3efdade
vaishnaviv <vaishnaviv@ideyalabs.com>
2024-11-23 15:12:19 +0530

    upadated some files

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const commentsData=[
     {
-        name:"Vaishnavi Velagapudi",
+        name:"Har Velagapudi",
         text:"This video is so Beautiful",
         reply:[
         ]
